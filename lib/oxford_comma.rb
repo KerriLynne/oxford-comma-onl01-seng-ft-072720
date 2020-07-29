50ab8bf6ce6b1b25ba3af867f17a7e17e81d5d5f
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-29 19:25:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 def oxford_comma(array)
-  array.join
-  array.join("and")
+  case array.size
+  when 0
+    ""
+  when 1
+    array.first
+  when 2
+    array.join(' and ')
+  else
+    [array[0..-2].join(', '), arr.last].join(', and ')
+  end
 end
